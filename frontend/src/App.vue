d092750a23a099cc45b26d30b3b681b2f67a9132
Kuba Krafka <jakub.krafka@datlab.eu>
2019-07-29 16:46:22 +0200

    reformatted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,58 +1,58 @@
 <template>
-  <div class="container-fluid" id="app">
-    <div class="row">
-      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-        <div class="sidebar-sticky">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <span data-feather="home"></span>
-                <router-link to="/">Home</router-link>
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
+    <div class="container-fluid" id="app">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span data-feather="home"></span>
+                                <router-link to="/">Home</router-link>
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
 
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <span data-feather="about"></span>
-                <router-link to="/about">About</router-link>
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span data-feather="about"></span>
+                                <router-link to="/about">About</router-link>
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-        <div
-          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
-        >
-          <h1 class="h2">Dashboard</h1>
-          <router-view />
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div
+                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+                >
+                    <h1 class="h2">Dashboard</h1>
+                    <router-view />
+                </div>
+            </main>
         </div>
-      </main>
     </div>
-  </div>
 </template>
 
 <style lang="scss">
 @import "src/scss/main";
 
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
