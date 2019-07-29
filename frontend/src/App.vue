150f13699a341876e2ceae255a5248bf31d07fb6
Kuba Krafka <jakub.krafka@datlab.eu>
2019-07-29 16:44:20 +0200

    skeleteon of dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div id="app">
-    <b-alert show>Default Alert</b-alert>
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <div class="container-fluid" id="app">
+    <div class="row">
+      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <span data-feather="home"></span>
+                <router-link to="/">Home</router-link>
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <span data-feather="about"></span>
+                <router-link to="/about">About</router-link>
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+        >
+          <h1 class="h2">Dashboard</h1>
+          <router-view />
+        </div>
+      </main>
     </div>
-    <router-view />
   </div>
 </template>
 
