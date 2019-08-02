6057226f2d46d476d1e805f77d737ddf844dcb0d
Kuba Krafka <jakub.krafka@datlab.eu>
2019-08-02 11:26:37 +0200

    added resource level check stats

--- backend/dqt/views.py
+++ backend/dqt/views.py
@@ -1,31 +1,33 @@
-from django.db.models import Count
-from django.http import HttpResponse
+from django.db.models import Count, Sum
+from django.http import JsonResponse
 
 from .models import ResourceLevelCheck
 
 
 def resource_level_stats(request, dataset_id):
-    distinct_checks = ResourceLevelCheck.objects.filter(dataset=dataset_id).values('check_name').distinct()
 
-    result = []
-    for check in distinct_checks:
-        ok_count = 0
-        failed_count = 0
-        na_count = 0
+    result = {}
 
-        counts = ResourceLevelCheck.objects.filter(dataset=dataset_id).filter(check_name = check["check_name"]).values("result").annotate(count=Count('id'))
-        for cnt in counts:
-            if cnt["result"] is None:
-                na_count = cnt["count"]
-            if cnt["result"] is True:
-                ok_count = cnt["count"]
-            if cnt["result"] is False:
-                failed_count = cnt["count"]
-        check_stats = {
-            "name": check["check_name"],
-            "ok": ok_count,
-            "failled": failed_count,
-            "na": na_count,
-        }
-        result.append(check_stats)
-    return HttpResponse(result)
+    counts = ResourceLevelCheck.objects.filter(dataset=dataset_id).values("check_name", "result").annotate(count=Count('id'))
+    for cnt in counts:
+        if cnt["check_name"] not in result:
+            result[cnt["check_name"]] = {
+                    "ok": 0,
+                    "failed": 0,
+                    "na": 0,
+                    "application_count": 0,
+                    "pass_count": 0,
+            }
+
+        if cnt["result"] is None:
+            result[cnt["check_name"]]["na"] = cnt["count"]
+        if cnt["result"] is True:
+            result[cnt["check_name"]]["ok"] = cnt["count"]
+        if cnt["result"] is False:
+            result[cnt["check_name"]]["failed"] = cnt["count"]
+
+    rates = ResourceLevelCheck.objects.filter(dataset=dataset_id).values("check_name").annotate(application_count=Sum('application_count')).annotate(pass_count=Sum('pass_count'))
+    for rate in rates:
+        result[rate["check_name"]]["application_count"] = rate["application_count"]
+        result[rate["check_name"]]["pass_count"] = rate["pass_count"]
+    return JsonResponse(result)
