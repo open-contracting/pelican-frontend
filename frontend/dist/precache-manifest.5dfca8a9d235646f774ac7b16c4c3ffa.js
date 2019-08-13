55c8cfa3685e8ae4f56e8ef1ed363151f07c9a97
Kuba Krafka <jakub.krafka@datlab.eu>
2019-08-13 15:39:07 +0200

    added dist folder

---
+++ frontend/dist/precache-manifest.5dfca8a9d235646f774ac7b16c4c3ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4bf5cb7cc8832336092b",
+    "url": "/js/chunk-vendors.650efb1c.js"
+  },
+  {
+    "revision": "c9bc28e30bb66c33a4ca",
+    "url": "/js/app.eefdcc4c.js"
+  },
+  {
+    "revision": "600ea09c5ba8978957e446566d50eb8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7339f8421ee46c09c2de676a7c38b18",
+    "url": "/img/ocp_logo.b7339f84.png"
+  },
+  {
+    "revision": "4d441802eabe151fb057b05aedcecaac",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "c9bc28e30bb66c33a4ca",
+    "url": "/css/app.771ddf64.css"
+  }
+];
