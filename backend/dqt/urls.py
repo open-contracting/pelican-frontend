ffbc1030132cc36d0d95ea2165e6a4094065829f
Miroslav Březík <mirek.brezik@gmail.com>
2019-09-05 12:44:08 +0200

    working filed_level_path_stats view

--- backend/dqt/urls.py
+++ backend/dqt/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from .api import (DatasetResource, DataItemResource, DatasetLevelCheckResource,
                   FieldLevelCheckResource, ProgressMonitorDatasetResource,
                   ResourceLevelCheckResource, ReportResource)
-from .views import dataset_stats, dataset_level_stats, resource_level_stats
+from .views import dataset_stats, dataset_level_stats, resource_level_stats, field_level_path_stats
 
 dataset_resource = DatasetResource()
 data_item_resource = DataItemResource()
@@ -20,6 +20,7 @@ urlpatterns = [
     path('api/dataset_stats/<dataset_id>', dataset_stats, name='dataset_stats'),
     path('api/resource_level_stats/<dataset_id>', resource_level_stats, name='resource_level_stats'),
     path('api/dataset_level_stats/<dataset_id>', dataset_level_stats, name='dataset_level_stats'),
+    path('api/field_level_path_stats/<dataset_id>/<path>', field_level_path_stats, name='field_level_path_stats'),
     url(r'^api/', include(dataset_resource.urls)),
     url(r'^api/', include(data_item_resource.urls)),
     url(r'^api/', include(progress_monitor_dataset_resource.urls)),
--- backend/dqt/views.py
+++ backend/dqt/views.py
@@ -1,10 +1,13 @@
 import random
+import simplejson as json
 
 from django.db.models import Count, Max, Min, Sum
 from django.http import JsonResponse
 from django.core import serializers
+from django.db import connections
 
-from .models import Dataset, DatasetLevelCheck, ResourceLevelCheck
+from .models import Dataset, DatasetLevelCheck, ResourceLevelCheck, Report
+from .tools import ReservoirSampler
 
 
 def dataset_stats(request, dataset_id):
@@ -97,4 +100,140 @@ def dataset_level_stats(request, dataset_id):
             "value": check.value,
             "meta": check.meta,
         }
+    return JsonResponse(result)
+
+
+def field_level_path_stats(request, dataset_id, path):
+    examples_cap = 20
+    result = None
+
+    with connections["data"].cursor() as cursor:
+        cursor.execute(
+            """
+            select data->%s
+            from report
+            where dataset_id = %s and type = 'field_level_check';
+            """, [path, dataset_id]
+        )
+        rows = cursor.fetchall()
+
+        if not rows:
+            return JsonResponse(
+                {
+                    "error": "no results for dataset_id: {}, path: '{}' combination".format(dataset_id, path)
+                }
+            )
+
+        result = rows[0][0]
+
+        # examples for this dataset_id, path combinations is cached
+        if result["examples_filled"]:
+            return JsonResponse(result)
+
+        # coverage
+        passed_coverage_sampler = ReservoirSampler(examples_cap)
+        failed_coverage_sampler = ReservoirSampler(examples_cap)
+        passed_coverage_checks_samplers = {}
+        failed_coverage_checks_samplers = {}
+        for check_name in result["coverage"]["checks"]:
+            passed_coverage_checks_samplers[check_name] = ReservoirSampler(examples_cap)
+            failed_coverage_checks_samplers[check_name] = ReservoirSampler(examples_cap)
+
+        cursor.execute(
+            """
+            select sub1.meta, sub1.path, sub1.check
+            from (
+                select sub2.meta,
+                        sub2.path->>'path' as path,
+                        jsonb_array_elements(sub2.path->'coverage'->'check_results') as check
+                from (
+                    select result->'meta' as meta, jsonb_array_elements(d.value) as path
+                    from field_level_check, jsonb_each(result->'checks') d
+                    where dataset_id = %s and d.key = %s
+                ) as sub2
+                where sub2.path->'coverage'->>'check_results' is not null
+            ) as sub1;
+            """, [dataset_id, path]
+        )
+
+        for row in cursor.fetchall():
+            example = {
+                "meta": row[0],
+                "path": row[1],
+                "result": row[2]
+            }
+
+            if example["result"]["result"]:
+                passed_coverage_sampler.process(example)
+                passed_coverage_checks_samplers[example["result"]["name"]].process(example)
+            else:
+                failed_coverage_sampler.process(example)
+                failed_coverage_checks_samplers[example["result"]["name"]].process(example)
+
+        result["coverage"]["passed_examples"] = passed_coverage_sampler.retrieve_samples()
+        result["coverage"]["failed_examples"] = failed_coverage_sampler.retrieve_samples()
+        for check_name, sampler in passed_coverage_checks_samplers.items():
+            result["coverage"]["checks"][check_name]["passed_examples"] = \
+                passed_coverage_checks_samplers[check_name].retrieve_samples()
+            result["coverage"]["checks"][check_name]["failed_examples"] = \
+                failed_coverage_checks_samplers[check_name].retrieve_samples()
+
+        # quality
+        passed_quality_sampler = ReservoirSampler(examples_cap)
+        failed_quality_sampler = ReservoirSampler(examples_cap)
+        passed_quality_checks_samplers = {}
+        failed_quality_checks_samplers = {}
+        for check_name in result["quality"]["checks"]:
+            passed_quality_checks_samplers[check_name] = ReservoirSampler(examples_cap)
+            failed_quality_checks_samplers[check_name] = ReservoirSampler(examples_cap)
+
+        cursor.execute(
+            """
+            select sub1.meta, sub1.path, sub1.check
+            from (
+                select sub2.meta,
+                        sub2.path->>'path' as path,
+                        jsonb_array_elements(sub2.path->'quality'->'check_results') as check
+                from (
+                    select result->'meta' as meta, jsonb_array_elements(d.value) as path
+                    from field_level_check, jsonb_each(result->'checks') d
+                    where dataset_id = %s and d.key = %s
+                ) as sub2
+                where sub2.path->'quality'->>'check_results' is not null
+            ) as sub1;
+            """, [dataset_id, path]
+        )
+
+        for row in cursor.fetchall():
+            example = {
+                "meta": row[0],
+                "path": row[1],
+                "result": row[2]
+            }
+
+            if example["result"]["result"]:
+                passed_quality_sampler.process(example)
+                passed_quality_checks_samplers[example["result"]["name"]].process(example)
+            else:
+                failed_quality_sampler.process(example)
+                failed_quality_checks_samplers[example["result"]["name"]].process(example)
+
+        result["quality"]["passed_examples"] = passed_quality_sampler.retrieve_samples()
+        result["quality"]["failed_examples"] = failed_quality_sampler.retrieve_samples()
+        for check_name, sampler in passed_quality_checks_samplers.items():
+            result["quality"]["checks"][check_name]["passed_examples"] = \
+                passed_quality_checks_samplers[check_name].retrieve_samples()
+            result["quality"]["checks"][check_name]["failed_examples"] = \
+                failed_quality_checks_samplers[check_name].retrieve_samples()
+
+        # saving examples
+        result["examples_filled"] = True
+        cursor.execute(
+            """
+            update report
+            set data = data || %s
+            where dataset_id = %s and type = 'field_level_check';
+            """, [json.dumps({path: result}), dataset_id]
+        )
+
     return JsonResponse(result)
