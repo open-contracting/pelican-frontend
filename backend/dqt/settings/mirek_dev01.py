ca4e1210e7377237e78b7b8037152e33fd9b9ae1
mirek <mirek.brezik@gmail.com>
2019-09-26 09:32:21 +0200

    resource_level_detail view optimized

---
+++ backend/dqt/settings/mirek_dev01.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=django,public'
+        },
+        'NAME': 'dqt',
+        'USER': 'dqt',
+        'PASSWORD': 'dqt',
+        'HOST': '127.0.0.1',
+        'PORT': '25001',
+    },
+
+    'data': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=development,public'
+        },
+        'NAME': 'dqt',
+        'USER': 'dqt',
+        'PASSWORD': 'dqt',
+        'HOST': '127.0.0.1',
+        'PORT': '25001',
+    },
+}
