cbcf625b90080e71dc2ba03eaf4f7a94146662e8
sandal <jan.hruby@datlab.eu>
2019-09-27 12:35:47 +0200

    production build

--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57961681aa988242af9acffcb4180452.js"
+  "/precache-manifest.684f6aef539749be2fd7c0eafd0fcb44.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
