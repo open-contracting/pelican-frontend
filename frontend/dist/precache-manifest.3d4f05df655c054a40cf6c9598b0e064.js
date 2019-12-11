eaa3b41971f9a13ae1d1274eff91a2a942cae225
mirek <mirek.brezik@gmail.com>
2019-12-11 14:37:58 +0100

    check page description text contrast increased

--- frontend/dist/precache-manifest.3d4f05df655c054a40cf6c9598b0e064.js
+++ frontend/dist/precache-manifest.3d4f05df655c054a40cf6c9598b0e064.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "1e32c015cdcb2089c58e",
-    "url": "/css/app.462f0db4.css"
+    "revision": "fbe7f1268bc00172d65d",
+    "url": "/css/app.b7fa040b.css"
   },
   {
-    "revision": "1e32c015cdcb2089c58e",
+    "revision": "fbe7f1268bc00172d65d",
     "url": "/js/app.6cf71823.js"
   },
   {
@@ -16,20 +16,16 @@ self.__precacheManifest = [
     "url": "/img/ocp_logo.9e00d24e.svg"
   },
   {
-    "revision": "a3badbb34fef9f3a7c5506c59971ff59",
+    "revision": "8556efc38eb48da0314d123152399ae9",
     "url": "/index.html"
-  },
-  {
-    "revision": "4fccb40f9a7c7a976e671cb128909bc6",
-    "url": "/fonts/GTEestiProDisplay-Bold.otf"
   },
   {
     "revision": "4d441802eabe151fb057b05aedcecaac",
     "url": "/favicon.png"
   },
   {
-    "revision": "dbb10b6b057824b8be624d1cbfd76d26",
-    "url": "/fonts/GTEestiProDisplay-LightItalic.otf"
+    "revision": "4fccb40f9a7c7a976e671cb128909bc6",
+    "url": "/fonts/GTEestiProDisplay-Bold.otf"
   },
   {
     "revision": "e3b5bde859d475c58834e7bf240c7396",
@@ -40,20 +36,24 @@ self.__precacheManifest = [
     "url": "/fonts/GTEestiProDisplay-BoldItalic.otf"
   },
   {
+    "revision": "6a145c940d222e0360e0f634d3a437cf",
+    "url": "/fonts/GTEestiProDisplay-Medium.otf"
+  },
+  {
+    "revision": "dbb10b6b057824b8be624d1cbfd76d26",
+    "url": "/fonts/GTEestiProDisplay-LightItalic.otf"
+  },
+  {
+    "revision": "89336101a3e5e0e5142fccd296e9125e",
+    "url": "/fonts/GTEestiProDisplay-RegularItalic.otf"
+  },
+  {
     "revision": "6af853bc1863b1d651939fe493d8ff6d",
     "url": "/fonts/GTEestiProDisplay-MediumItalic.otf"
   },
   {
     "revision": "4ac4cbee5a45244b78aa92dc898d4300",
     "url": "/fonts/GTEestiProDisplay-Regular.otf"
-  },
-  {
-    "revision": "6a145c940d222e0360e0f634d3a437cf",
-    "url": "/fonts/GTEestiProDisplay-Medium.otf"
-  },
-  {
-    "revision": "89336101a3e5e0e5142fccd296e9125e",
-    "url": "/fonts/GTEestiProDisplay-RegularItalic.otf"
   },
   {
     "revision": "11c703f172586daa4143b4e8b32991a9",
@@ -64,10 +64,6 @@ self.__precacheManifest = [
     "url": "/fonts/GTEestiProDisplay-ThinItalic.otf"
   },
   {
-    "revision": "1d7924dbe5a122ed5f1b6a96bf4b9c63",
-    "url": "/fonts/GTEestiProDisplay-UltraLight.otf"
-  },
-  {
     "revision": "e4f44de9d4fa30b8549e4809011bfdd3",
     "url": "/fonts/GTEestiProDisplay-UltraBold.otf"
   },
@@ -76,12 +72,16 @@ self.__precacheManifest = [
     "url": "/fonts/GTEestiProDisplay-UltraBoldItalic.otf"
   },
   {
-    "revision": "cad599a0994fc34d9763c5d2dba361be",
-    "url": "/img/unsufficient_data.png"
-  },
-  {
     "revision": "412f1329680191bd591f8f875c92ce6a",
     "url": "/fonts/GTEestiProDisplay-UltraLightItalic.otf"
+  },
+  {
+    "revision": "1d7924dbe5a122ed5f1b6a96bf4b9c63",
+    "url": "/fonts/GTEestiProDisplay-UltraLight.otf"
+  },
+  {
+    "revision": "cad599a0994fc34d9763c5d2dba361be",
+    "url": "/img/unsufficient_data.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e1ef983c8328aaafbae1b78d5048ee6.js"
+  "/precache-manifest.3d4f05df655c054a40cf6c9598b0e064.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
