6f2a7c213fa1818c9b6e2d49fbf03d61d12633d4
Kuba Krafka <jakub@krafka.net>
2020-07-20 14:19:38 +0200

    first version of template merge

--- gdocs/main.py
+++ gdocs/main.py
@@ -3,53 +3,64 @@ import pickle
 import os.path
 import json
 
+import lxml.etree as etree
+from zipfile import ZipFile
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from gdocs.utils import init
-from gdocs.utils import copy
 from gdocs.utils import get_tags
-from gdocs.utils import get_content
-from gdocs.utils import update
+from gdocs.utils import download
+from gdocs.utils import upload
+from gdocs.utils import copy_zip
+from gdocs.utils import get_template
+from gdocs.utils import get_main_template
+from gdocs.utils import merge_templates
+from gdocs.utils import get_tags
+
+
+from odf.opendocument import load
 
 # The ID of a sample document.
-TEMPLATE_DOCUMENT_ID = '1qSWgaomfyd6serjz70dGHlGG7TsMQyUTkd84o4nSAVk'
-
-COPY_ID = "1l7YriELY6Jb2ziBE1txRVnuUr7EzuUgjsVFFPOorNLg"
+TEMPLATE_DOCUMENT_ID = '1kES07KZRc7VcEfh0cCWZvHpwIidCMlgvD0XRECoO-lY'
 
 SUBTEMPLATE_ID = "1PQzxjC-BEYF4MdgOu0uZFc6cVq3ZKf_4B_pXDduzH3Q"
+
+FOLDER_ID = "1yLTCRV3yoBM5Goc93SaO4irxnd0iapnK"
 
 
 def main():
     init()
 
-    # report_id = copy(TEMPLATE_DOCUMENT_ID, "Copy test")
-    # print(report_id)
-    content = get_content(SUBTEMPLATE_ID)
+    main_template = get_main_template(TEMPLATE_DOCUMENT_ID)
 
-    requests = []
-    for node in content:
-        if 'paragraph' in node:
-            elements = node.get('paragraph').get('elements')
+    main_template = process_template(main_template, None)
 
-            for element in elements:
-                text_run = element.get('textRun')
-                if not text_run:
-                    continue
-                insert = {
-                    "insertText": {
-                        "location": {
-                            "index": 15
-                        },
-                        "text": text_run.get('content')
-                    }
-                }
+    print(etree.tostring(main_template))
 
-                requests.append(insert)
+    upload(FOLDER_ID, TEMPLATE_DOCUMENT_ID, "test_upload_4", main_template)
+    print("ok")
 
-    print(update(TEMPLATE_DOCUMENT_ID, requests))
 
-    # print(json.dumps(content, indent=4, sort_keys=True))
+def process_template(template, parent_tag):
+    tags = get_tags(template)
+    for tag in tags:
+        if tag["name"] == "template":
+            template_id = ""
+            for param in tag["params"]:
+                if param["name"] == "id":
+                    template_id = param["value"]
+
+            if not template_id:
+                raise ValueError("Missing template id in {}".format(tag["full"]))
+
+            sub_template = get_template(template_id)
+
+            sub_template = process_template(sub_template, tag)
+
+            template = merge_templates(template, sub_template, tag["full"])
+
+    return template
 
 
 if __name__ == '__main__':
--- gdocs/utils.py
+++ gdocs/utils.py
@@ -2,9 +2,14 @@ from __future__ import print_function
 import pickle
 import os.path
 import re
+import lxml.etree as etree
+import shortuuid
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
+from zipfile import ZipFile
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/documents']
@@ -47,120 +52,153 @@ def create_or_refresh_token():
             pickle.dump(creds, token)
 
 
-def copy(source_id, new_title):
-    """Copy document with source_id, set new title and return the id of a new document"""
-    body = {
-        'name': new_title
+def download(source_id, path):
+    """Downloads document with source_id"""
+    drive_response = drive_service.files().export(
+        fileId=source_id, mimeType="application/vnd.oasis.opendocument.text").execute()
+
+    f = open(path, "wb")
+    f.write(drive_response)
+    f.close()
+
+    return drive_response
+
+
+def upload(folder_id, template_id, file_name, content):
+    path = "/tmp/{}_out".format(template_id)
+    with ZipFile(path, mode="a") as out_zip:
+        out_zip.writestr('content.xml', etree.tostring(content))
+
+    file_metadata = {
+        'name': file_name,
+        'mimeType': 'application/vnd.google-apps.document',
+        "parents": [folder_id]
     }
 
-    drive_response = drive_service.files().copy(
-        fileId=source_id, body=body).execute()
-    copy_id = drive_response.get('id')
+    media = MediaFileUpload(path,
+                            mimetype='application/vnd.oasis.opendocument.text',
+                            resumable=True)
 
-    return copy_id
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media).execute()
 
-
-def update(doc_id, requests):
-    """Performs batch update in a document"""
-
-    result = service.documents().batchUpdate(
-        documentId=doc_id, body={'requests': requests}).execute()
-
-    return result
+    print("File ID: {}".format(file.get('id')))
 
 
-def get_content(document_id):
-    """Document content"""
-    document = service.documents().get(documentId=document_id).execute()
-    return document.get('body').get('content')
+def copy_zip(source, target):
+    with ZipFile(source, 'r') as zipread:
+        with ZipFile(target, 'w') as zipwrite:
+            for item in zipread.infolist():
+                if item.filename not in ("content.xml"):
+                    data = zipread.read(item.filename)
+                    zipwrite.writestr(item, data)
 
 
-def get_plain_text(document_id):
-    """Returns plain text from the document"""
-    document = service.documents().get(documentId=document_id).execute()
-    content = document.get('body').get('content')
-    return read_structural_elements(content)
+def get_main_template(template_id):
+    path = "/tmp/{}".format(template_id)
+    download(template_id, path)
+
+    copy_zip(path, path + "_out")
+
+    with ZipFile(path) as myzip:
+        with myzip.open('content.xml') as content:
+            return etree.parse(content).getroot()
 
 
-def get_tags(document_id):
-    content = get_plain_text(document_id)
+def get_template(template_id):
+    path = "/tmp/{}".format(template_id)
+    download(template_id, path)
 
-    lines = re.findall("{%.*%}", content)
+    with ZipFile(path) as myzip:
+        with myzip.open('content.xml') as content:
+            return etree.parse(content).getroot()
+
+
+def get_template_content(template):
+    for node in template.xpath('//office:text',
+                               namespaces={'office': 'urn:oasis:names:tc:opendocument:xmlns:office:1.0'}):
+        return node
+
+
+def get_template_styles(template):
+    for node in template.xpath('//office:automatic-styles',
+                               namespaces={'office': 'urn:oasis:names:tc:opendocument:xmlns:office:1.0'}):
+        return node
+
+
+def merge_templates(main_template, sub_template, location):
+    prefix = shortuuid.uuid()
+    sub_template_content = get_template_content(sub_template)
+    for location_node in main_template.xpath('.//*[contains(text(),"' + location + '")]'):
+        parent = location_node.getparent()
+
+        previous = parent
+        for child in sub_template_content:
+            if not "sequence-decls" in child.tag:
+                # lets rename all paragraph styles to standard
+                for attrib in child.attrib:
+                    if "style-name" in attrib:
+                        child.set(attrib, "Standard")
+
+                for node in child:
+                    for attrib in node.attrib:
+                        if attrib.endswith("style-name"):
+                            node.set(attrib, prefix + node.get(attrib))
+
+                previous.addnext(child)
+                previous = child
+
+        # remove the template node
+        parent.remove(location_node)
+
+    main_template = merge_template_styles(main_template, sub_template, prefix)
+    return main_template
+
+
+def merge_template_styles(main_template, sub_template, prefix):
+    sub_template_styles = get_template_styles(sub_template)
+
+    for main_style_node in main_template.xpath(
+            '//office:automatic-styles', namespaces={'office': 'urn:oasis:names:tc:opendocument:xmlns:office:1.0'}):
+
+        for sub_style in sub_template_styles:
+            # lets rename all paragraph styles to standard
+            for attrib in sub_style.attrib:
+                if attrib.endswith("name"):
+                    sub_style.set(attrib, prefix + sub_style.get(attrib))
+
+            main_style_node.append(sub_style)
+
+    return main_template
+
+
+def get_tags(template):
     tags = []
 
-    for line in lines:
-        tag = {}
-        tag["full"] = line
+    for node in template.xpath('.//text()'):
 
-        line = line[2:-2]
+        lines = re.findall("{%.*%}", node)
 
-        chunks = line.split()
-        tag["name"] = chunks[0]
+        for line in lines:
+            tag = {}
+            tag["full"] = line
 
-        params = []
-        if len(chunks) > 1:
-            for chunk in chunks[1:]:
-                parts = chunk.split(':|')
-                param = {}
-                param["name"] = parts[0]
-                if len(parts) > 1:
-                    param["value"] = parts[1][:-1]
+            line = line[2:-2]
 
-                params.append(param)
-        tag["params"] = params
-        tags.append(tag)
+            chunks = line.split()
+            tag["name"] = chunks[0]
+
+            params = []
+            if len(chunks) > 1:
+                for chunk in chunks[1:]:
+                    parts = chunk.split(':|')
+                    param = {}
+                    param["name"] = parts[0]
+                    if len(parts) > 1:
+                        param["value"] = parts[1][:-1]
+
+                    params.append(param)
+            tag["params"] = params
+            tags.append(tag)
 
     return tags
-
-
-def get_tag_start_index(content, search_text):
-    """Returns the paragraph elements with text containing search_text.
-
-        Args:
-            content: a list of elements from a Google Doc.
-            search_text:
-    """
-    text_run = element.get('textRun')
-    if not text_run:
-        return ''
-    return text_run.get('content')
-
-
-def read_paragraph_element(element):
-    """Returns the text in the given ParagraphElement.
-
-        Args:
-            element: a ParagraphElement from a Google Doc.
-    """
-    text_run = element.get('textRun')
-    if not text_run:
-        return ''
-    return text_run.get('content')
-
-
-def read_structural_elements(elements):
-    """Recurses through a list of Structural Elements to read a document's text where text may be
-        in nested elements.
-
-        Args:
-            elements: a list of Structural Elements.
-    """
-    text = ''
-    for value in elements:
-        if 'paragraph' in value:
-            elements = value.get('paragraph').get('elements')
-            for elem in elements:
-                text += read_paragraph_element(elem)
-        elif 'table' in value:
-            # The text in table cells are in nested Structural Elements and tables may be
-            # nested.
-            table = value.get('table')
-            for row in table.get('tableRows'):
-                cells = row.get('tableCells')
-                for cell in cells:
-                    text += read_structural_elements(cell.get('content'))
-        elif 'tableOfContents' in value:
-            # The text in the TOC is also in a Structural Element.
-            toc = value.get('tableOfContents')
-            text += read_structural_elements(toc.get('content'))
-    return text
