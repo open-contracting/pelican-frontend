ccb6ebc8f5d138341a1cfe35a10a0dcfc7f2f828
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-04 17:49:39 -0400

    settings: Copy LOGGING configuration from data-registry

--- backend/dqt/settings/docker.py
+++ backend/dqt/settings/docker.py
@@ -39,9 +39,57 @@ RABBIT = {
 }
 
 GDOCS_TEMPLATES = {
-    "DEFAULT_BASE_TEMPLATE" : os.getenv("DEFAULT_BASE_TEMPLATE"),
-    "DEFAULT_DATASET_TEMPLATE" :  os.getenv("DEFAULT_DATASET_TEMPLATE"),
-    "DEFAULT_FIELD_TEMPLATE":  os.getenv("DEFAULT_FIELD_TEMPLATE"),
-    "DEFAULT_RESOURCE_TEMPLATE":  os.getenv("DEFAULT_RESOURCE_TEMPLATE"),
-    "DEFAULT_ERROR_TEMPLATE" :  os.getenv("DEFAULT_ERROR_TEMPLATE"),
+    "DEFAULT_BASE_TEMPLATE": os.getenv("DEFAULT_BASE_TEMPLATE"),
+    "DEFAULT_DATASET_TEMPLATE": os.getenv("DEFAULT_DATASET_TEMPLATE"),
+    "DEFAULT_FIELD_TEMPLATE": os.getenv("DEFAULT_FIELD_TEMPLATE"),
+    "DEFAULT_RESOURCE_TEMPLATE": os.getenv("DEFAULT_RESOURCE_TEMPLATE"),
+    "DEFAULT_ERROR_TEMPLATE": os.getenv("DEFAULT_ERROR_TEMPLATE"),
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.server": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "data_registry": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+        "exporter": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+        "cbom": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+        "scrape-task": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
 }
