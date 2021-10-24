e580b61f23244b7c8e2b7bac80844a3e7eae4449
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-23 23:21:50 -0400

    routers: Update to match example in Django documentation and to correspond to recent changes in models

--- backend/dqt/routers.py
+++ backend/dqt/routers.py
@@ -1,21 +1,24 @@
+# https://docs.djangoproject.com/en/3.2/topics/db/multi-db/#an-example
 class DbRouter:
+    route_app_labels = {"dqt"}
+
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == "data":
+        if model._meta.app_label in self.route_app_labels:
             return "data"
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == "data":
+        if model._meta.app_label in self.route_app_labels:
             return "data"
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label == "data" or obj2._meta.app_label == "data":
+        if obj1._meta.app_label in self.route_app_labels or obj2._meta.app_label in self.route_app_labels:
             return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if db == "data":
+        if app_label in self.route_app_labels:
             return False
 
         return None
