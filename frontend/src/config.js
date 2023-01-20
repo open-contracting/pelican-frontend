d2695fca77ec7f456463c6531baddd234522d182
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-01-20 12:12:56 -0500

    docs: Add comments and group API endpoints for easier comparison between JS and Python

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,16 +2,21 @@ var config = {
     apiBaseUrl: process.env.VUE_APP_API_BASE_URL,
     apiEndpoints: {
         dataset: "api/dataset",
+        dataItem: "api/data_item",
+        // Stats
+        fieldStats: "api/field_level_stats",
         resourceLevelStats: "api/resource_level_stats",
-        resourceLevelCheckDetail: "api/resource_level_detail",
         datasetLevelStats: "api/dataset_level_stats",
         timeVarianceLevelStats: "api/time_variance_level_stats",
-        dataItem: "api/data_item",
-        fieldStats: "api/field_level_stats",
+        // Detail
         fieldDetail: "api/field_level_detail",
-        datasetDistinctValues: "api/dataset_distinct_values",
-        createDatasetFilter: "datasets/{id}/filter/",
+        resourceLevelCheckDetail: "api/resource_level_detail",
+        // Rest
         datasetFilterItems: "api/dataset_filter_items",
+        datasetDistinctValues: "api/dataset_distinct_values",
+        // Controller
+        createDatasetFilter: "datasets/{id}/filter/",
+        // Exporter
         createDatasetReport: "api/generate_report"
     }
 };
