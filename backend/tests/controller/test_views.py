39bce10e3d031d80d0ad297b134b4aa7eecfa64b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-01-20 19:05:22 -0500

    test: Add test for data_items/<pk>

--- backend/tests/controller/test_views.py
+++ backend/tests/controller/test_views.py
@@ -5,7 +5,20 @@ from tests import TestCase
 
 
 class ViewsTests(TestCase):
-    def test_list(self):
+    def test_data_items_list_not_implemented(self):
+        response = self.client.get("/data_items/")
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_data_items_retrieve(self):
+        dataset = self.create(Dataset, name="parent")
+        data_item = self.create(DataItem, dataset=dataset, data={"parties": {"id": 1}})
+        response = self.client.get(f"/data_items/{data_item.pk}/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"data": {"parties": {"id": 1}}, "id": 1})
+
+    def test_datasets_list(self):
         dataset = self.create(Dataset, name="parent")
         filtered = self.create(Dataset, name="child")
         self.create(ProgressMonitorDataset, dataset=dataset, phase="CHECKED", state="OK")
@@ -45,7 +58,7 @@ class ViewsTests(TestCase):
             ],
         )
 
-    def test_retrieve(self):
+    def test_datasets_retrieve(self):
         dataset = self.create(Dataset, name="parent")
         filtered = self.create(Dataset, name="child")
         self.create(ProgressMonitorDataset, dataset=dataset, phase="CHECKED", state="OK")
@@ -71,21 +84,21 @@ class ViewsTests(TestCase):
         )
 
     @patch("controller.views.publish")
-    def test_create_invalid(self, publish):
+    def test_datasets_create_invalid(self, publish):
         response = self.client.post("/datasets/", {"name": "anything", "collection_id": "xxx"}, "application/json")
 
         self.assertEqual(response.status_code, 400)
         publish.assert_not_called()
 
     @patch("controller.views.publish")
-    def test_create_no_values(self, publish):
+    def test_datasets_create_no_values(self, publish):
         response = self.client.post("/datasets/", {}, "application/json")
 
         self.assertEqual(response.status_code, 400)
         publish.assert_not_called()
 
     @patch("controller.views.publish")
-    def test_create(self, publish):
+    def test_datasets_create(self, publish):
         response = self.client.post(
             "/datasets/", {"name": "anything", "collection_id": "123", "xxx": "xxx"}, "application/json"
         )
@@ -94,14 +107,14 @@ class ViewsTests(TestCase):
         publish.assert_called_once_with({"name": "anything", "collection_id": 123}, "ocds_kingfisher_extractor_init")
 
     @patch("controller.views.publish")
-    def test_filter_invalid(self, publish):
+    def test_datasets_filter_invalid(self, publish):
         response = self.client.post("/datasets/123/filter/", {"buyer": "xxx"}, "application/json")
 
         self.assertEqual(response.status_code, 400)
         publish.assert_not_called()
 
     @patch("controller.views.publish")
-    def test_filter_no_values(self, publish):
+    def test_datasets_filter_no_values(self, publish):
         response = self.client.post("/datasets/123/filter/", {}, "application/json")
 
         self.assertEqual(response.status_code, 202)
@@ -110,7 +123,7 @@ class ViewsTests(TestCase):
         )
 
     @patch("controller.views.publish")
-    def test_filter(self, publish):
+    def test_datasets_filter(self, publish):
         response = self.client.post("/datasets/123/filter/", {"buyer": ["MOF"], "xxx": "xxx"}, "application/json")
 
         self.assertEqual(response.status_code, 202)
@@ -119,41 +132,41 @@ class ViewsTests(TestCase):
         )
 
     @patch("controller.views.publish")
-    def test_destroy(self, publish):
+    def test_datasets_destroy(self, publish):
         response = self.client.delete("/datasets/123/")
 
         self.assertEqual(response.status_code, 202)
         publish.assert_called_once_with({"dataset_id": 123}, "wiper_init")
 
-    def test_find_by_name_no_name(self):
+    def test_datasets_find_by_name_no_name(self):
         self.create(Dataset, name="anything")
         response = self.client.get("/datasets/find_by_name/")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {})
 
-    def test_find_by_name_no_match(self):
+    def test_datasets_find_by_name_no_match(self):
         self.create(Dataset, name="anything")
         response = self.client.get("/datasets/find_by_name/?name=other")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {})
 
-    def test_find_by_name(self):
+    def test_datasets_find_by_name(self):
         dataset = self.create(Dataset, name="anything")
         response = self.client.get("/datasets/find_by_name/?name=anything")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {"id": dataset.pk})
 
-    def test_status_no_progress(self):
+    def test_datasets_status_no_progress(self):
         dataset = self.create(Dataset, name="anything")
         response = self.client.get(f"/datasets/{dataset.pk}/status/")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {})
 
-    def test_status_no_values(self):
+    def test_datasets_status_no_values(self):
         dataset = self.create(Dataset, name="anything")
         self.create(ProgressMonitorDataset, dataset=dataset)
         response = self.client.get(f"/datasets/{dataset.pk}/status/")
@@ -161,7 +174,7 @@ class ViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {"phase": None, "state": None})
 
-    def test_status(self):
+    def test_datasets_status(self):
         dataset = self.create(Dataset, name="anything")
         self.create(ProgressMonitorDataset, dataset=dataset, phase="CHECKED", state="OK")
         response = self.client.get(f"/datasets/{dataset.pk}/status/")
@@ -169,21 +182,21 @@ class ViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {"phase": "CHECKED", "state": "OK"})
 
-    def test_metadata_no_values(self):
+    def test_datasets_metadata_no_values(self):
         dataset = self.create(Dataset, name="anything")
         response = self.client.get(f"/datasets/{dataset.pk}/metadata/")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {})
 
-    def test_metadata(self):
+    def test_datasets_metadata(self):
         dataset = self.create(Dataset, name="anything", meta={"collection_metadata": {"ocid_prefix": "ocds-213czf"}})
         response = self.client.get(f"/datasets/{dataset.pk}/metadata/")
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {"ocid_prefix": "ocds-213czf"})
 
-    def test_coverage(self):
+    def test_datasets_coverage(self):
         dataset = self.create(Dataset, name="anything")
         data_item = self.create(DataItem, dataset=dataset, data={"parties": {"id": 1}})
         self.create(FieldLevelCheck, dataset=dataset, data_item=data_item, result={"checks": {"parties.id": [{}]}})
@@ -211,14 +224,14 @@ class ViewsTests(TestCase):
             },
         )
 
-    def test_status_no_dataset(self):
+    def test_datasets_status_no_dataset(self):
         response = self.client.get("/datasets/123/status/")
         self.assertEqual(response.status_code, 404)
 
-    def test_metadata_no_dataset(self):
+    def test_datasets_metadata_no_dataset(self):
         response = self.client.get("/datasets/123/metadata/")
         self.assertEqual(response.status_code, 404)
 
-    def test_coverage_no_dataset(self):
+    def test_datasets_coverage_no_dataset(self):
         response = self.client.get("/datasets/123/coverage/")
         self.assertEqual(response.status_code, 404)
