6248b7a9ab0d40e07707f6939f42a33f1ebd9988
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-07-02 23:28:42 -0400

    build: Upgrade yapw, to handle RabbitMQ restarts. Upgrade to Python 3.11

--- backend/api/rabbitmq.py
+++ backend/api/rabbitmq.py
@@ -1,13 +1,9 @@
 from django.conf import settings
-from yapw import clients
-
-
-class Publisher(clients.Durable, clients.Blocking, clients.Base):
-    pass
+from yapw.clients import Blocking
 
 
 def publish(*args, **kwargs):
-    client = Publisher(url=settings.RABBIT_URL, exchange=settings.RABBIT_EXCHANGE_NAME)
+    client = Blocking(url=settings.RABBIT_URL, exchange=settings.RABBIT_EXCHANGE_NAME)
     try:
         client.publish(*args, **kwargs)
     finally:
