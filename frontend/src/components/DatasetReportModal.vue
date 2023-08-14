open-contracting/pelican-frontend

commit ff00c21e179b3cc3700834b1262c6cb334298180

Works, WIP

frontend/src/components/DatasetReportModal.vue
@@ -2,21 +2,44 @@
     <span class="just_holder">
         <Loader v-if="isSubmitting"></Loader>
         <span v-if="submitStatus != null">
-            <span v-if="submitStatus == 'ok'">
+            <span v-if="(submitStatus == 'ok') && (!failedTags)">
                 <b-alert class="submit-result" variant="success" show>
                     <span>
                         {{ $t("datasetReport.status.ok") }}
                     </span>
-                    <span>
-                        <a class="variant-success" v-on:click.stop.prevent="retry" href="#">
-                            <font-awesome-icon :icon="['fas', 'redo-alt']" />
-                        </a>
-                    </span>
                 </b-alert>
-                <span class="info_prefix">{{ $t("datasetReport.link") }}:&nbsp;</span>
+                <span class="info_prefix margin_bottom">{{ $t("datasetReport.link") }}:&nbsp;</span>
                 <a :href="'https://docs.google.com/document/d/' + submitData.file_id" target="_blank">
                     {{ "https://docs.google.com/document/d/" + submitData.file_id }}
                 </a>
+                <b-row class="buttons">
+                    <b-col>
+                        <button class="variant-success btn-success" v-on:click.stop.prevent="retry" href="#">
+                            <font-awesome-icon :icon="['fas', 'redo-alt']" class="icon" />
+                            {{ $t("tryAgain") }}
+                        </button>
+                    </b-col>
+                    <b-col class="right-align">
+                        <button class="variant-success btn-success" v-on:click.stop.prevent="$emit('close')" href="#">
+                            <font-awesome-icon :icon="['fas', 'window-close']" class="icon" />
+                            {{ $t("close") }}
+                        </button>
+                    </b-col>
+                </b-row>
+            </span>
+            <span v-if="(submitStatus == 'ok') && (failedTags)">
+                <b-alert class="submit-result" variant="warning" show>
+                    <span>
+                        {{ $t("datasetReport.status.warning") }}
+                    </span>
+                </b-alert>
+
+                <div class="margin_bottom">
+                    <span class="info_prefix">{{ $t("datasetReport.link") }}:&nbsp;</span>
+                    <a :href="'https://docs.google.com/document/d/' + submitData.file_id" target="_blank">
+                        {{ "https://docs.google.com/document/d/" + submitData.file_id }}&nbsp;
+                    </a>
+                </div>
             </span>
             <!-- TODO -->
             <span v-if="submitStatus == 'template_error'">
@@ -87,6 +110,30 @@
                     </b-col>
                     <b-col class="right-align">
                         <button class="variant-danger btn-danger" v-on:click.stop.prevent="$emit('close')" href="#">
+                            <font-awesome-icon :icon="['fas', 'window-close']" class="icon" />
+                            {{ $t("close") }}
+                        </button>
+                    </b-col>
+                </b-row>
+            </span>
+            <span v-if="failedTags != [] && failedTags != null">
+    
+                <span class="info_prefix">{{ $t("datasetReport.warningList") }}:&nbsp;</span>
+                <ul>
+                    <li v-for="(tag, index) in failedTags" :key="index">
+                        {{ tag }}
+                    </li>
+                </ul>
+                <span class="info_prefix margin_bottom">{{ $t("datasetReport.warningEnd") }}&nbsp;</span>
+                <b-row class="buttons">
+                    <b-col>
+                        <button class="variant-warning btn-warning" v-on:click.stop.prevent="retry" href="#">
+                            <font-awesome-icon :icon="['fas', 'redo-alt']" class="icon" />
+                            {{ $t("tryAgain") }}
+                        </button>
+                    </b-col>
+                    <b-col class="right-align">
+                        <button class="variant-warning btn-warning" v-on:click.stop.prevent="$emit('close')" href="#">
                             <font-awesome-icon :icon="['fas', 'window-close']" class="icon" />
                             {{ $t("close") }}
                         </button>
@@ -168,7 +215,8 @@ export default {
             submitStatus: null,
             submitData: null,
             documentLink: null,
-            errorMessage: null
+            errorMessage: null,
+            failedTags: null
         };
     },
     components: { Loader },
@@ -192,6 +240,9 @@ export default {
             axios
                 .post(CONFIG.apiBaseUrl + CONFIG.apiEndpoints.createDatasetReport, data)
                 .then(response => {
+                    if (response.data.failed_tags.length == 0) this.failedTags = null;
+                    else this.failedTags = response.data.failed_tags;
+
                     if (response.status == 200) {
                         this.submitStatus = response.data.status;
                         this.submitData = response.data.data;
@@ -257,6 +308,10 @@ export default {
     padding-bottom: 30px;
 }
 
+.margin_bottom {
+    margin-bottom: 1em;
+}
+
 .base_input,
 .base_input:-internal-autofill-selected {
     width: 100%;
@@ -296,7 +351,9 @@ export default {
     align-items: flex-end;
 }
 
-.btn-danger {
+.btn-danger,
+.btn-warning,
+.btn-success {
     border-radius: 5px;
     border: 1px solid $na_color;
     padding: 1em;

backend/dqt/tools/errors.py
@@ -30,3 +30,27 @@ class TagError(Exception):
             "full_tag": self.full_tag,
             "template_id": self.template_id,
         }
+
+#Error used for singaling and handling noncomputed checks while generating report
+class CheckNotComputedError(Exception):
+    def __init__(self, reason, check=None):
+        self.reason = reason
+        self.check = check
+
+    def set_reason(self, reason):
+        self.reason = reason
+
+    def set_check(self, check):
+        self.check = check
+
+    def is_set(self):
+        return None not in (self.reason, self.full_tag, self.template_id)
+
+    def get_check(self):
+        return self.check    
+
+    def as_dict(self):
+        return {
+            "reason": self.reason,
+            "check": self.check,
+        }

backend/dqt/tools/tags/leaf_tags/examples_leaf_tag_factory.py
@@ -27,8 +27,9 @@ def generate_examples_leaf_tag(key):
             max_count = self.get_param("max")
             if max_count is None:
                 max_count = len(data[key])
-
-            examples = random.sample(data[key], k=int(max_count))
+            
+            ### Choosing examples, if max count bigger than sample size, choosing all the samples instead
+            examples = random.sample(data[key], k=min(len(data[key]), int(max_count)))
 
             mode = self.get_param("mode")
             if mode is None:

backend/dqt/tools/tags/leaf_tags/field/failed_examples.py
@@ -38,7 +38,9 @@ class FailedExamplesLeafTag(LeafTag):
         if max_count is None:
             max_count = len(all_examples)
 
-        examples = random.sample(all_examples, k=int(max_count))
+           
+        ### Choosing examples, if max count bigger than sample size, choosing all the samples instead
+        examples = random.sample(all_examples, k=min(len(all_examples), int(max_count)))
 
         mode = self.get_param("mode")
         if mode is None:

backend/dqt/tools/tags/tag.py
@@ -1,10 +1,12 @@
 import copy
 import re
+from pyasn1.type.univ import Null
 
 import shortuuid
-from dqt.tools.errors import TagError
+from dqt.tools.errors import CheckNotComputedError, TagError
 from dqt.tools.misc import terms_enumeration
 from lxml import etree
+
 
 
 # General tag class representing all kinds of tags, that can occur (TemplateTag, LeafTag, ...)
@@ -186,6 +188,20 @@ class TemplateTag(Tag):
                 if child.tail:
                     child.tail = child.tail.replace(full_tag, str(value))
 
+    # replaces all occurrences of full_tag with string value in the template, formates as error
+    def set_error(self, value, full_tag):
+        nodes = self.template.xpath(TemplateTag.FULL_TAG_LOCATION_XPATH.format(full_tag=full_tag))
+        for node in nodes:           
+            if node.text:
+                node.text = node.text.replace(full_tag, str(value))
+            for child in node:
+                child.set("color", "red")
+                if child.text:
+                    child.text = child.text.replace(full_tag, str(value))  # probably unnecessary
+                if child.tail:
+                    child.tail = child.tail.replace(full_tag, str(value))        
+            node.set("color", "red")                    
+
     # replaces all occurrences of full_tag with etree._Element value in the template
     # automatically wraps an element in <text:p>
     # if the node containing full_tag is not a <text:p> element the new element is added to the root of the document
@@ -331,6 +347,7 @@ class TemplateTag(Tag):
     # searches for tags in the template and sets their params
     def get_tags_mapping(self):
         tags_mapping = {}
+        failed_tags = []
 
         full_tags = set()
         texts = self.template.xpath(TemplateTag.TAG_EXPRESSION_XPATH)
@@ -372,7 +389,13 @@ class TemplateTag(Tag):
                             er.set_full_tag(full_tag)
                             er.set_template_id(self.get_param("template"))
                         raise er
-                tag.finalize_params()
+                try:
+                    tag.finalize_params()
+                except CheckNotComputedError as er: 
+                    failed_tags.append(er.check)
+                    tags_mapping[full_tag] = generate_error_leaf_tag("Check " + er.check +" was not computed. Please check your dataset.")    
+                    continue
+                       
             else:
                 try:
                     tag_chaining = TagChaining(self, tag_expression)
@@ -385,7 +408,7 @@ class TemplateTag(Tag):
 
             tags_mapping[full_tag] = tag
 
-        return tags_mapping
+        return tags_mapping, failed_tags
 
     # recursive method that calls sub tags' validate_and_process methods and incorporates the result in the template
     def validate_and_process(self, data):
@@ -394,9 +417,25 @@ class TemplateTag(Tag):
         self.template = self.gdocs.get_template(self.get_param("template"))
         new_data = self.prepare_data(data)
 
-        tags_mapping = self.get_tags_mapping()
+        
+        tags_mapping,failed_tags = self.get_tags_mapping()
+        
         for full_tag, tag in tags_mapping.items():
-            if isinstance(tag, LeafTag):
+            if isinstance(tag, ErrorLeafTag): 
+                try:
+                    result = tag.validate_and_process(new_data)
+                except TagError as er:
+                    if not er.is_set():
+                        er.set_full_tag(full_tag)
+                        er.set_template_id(self.get_param("template"))
+                    raise er
+                if isinstance(result, str):
+                    self.set_error(result, full_tag)
+                else:
+                    raise ValueError(
+                        "ErrorLeafTag's process_tag method must return 'str'."
+                    )
+            elif isinstance(tag, LeafTag):
                 try:
                     result = tag.validate_and_process(new_data)
                 except TagError as er:
@@ -419,16 +458,21 @@ class TemplateTag(Tag):
 
             elif isinstance(tag, TemplateTag):
                 try:
-                    result = tag.validate_and_process(new_data)
+                    result, failed = tag.validate_and_process(new_data)
+                    failed_tags = failed_tags + failed
                 except TagError as er:
                     if not er.is_set():
                         er.set_full_tag(full_tag)
                         er.set_template_id(self.get_param("template"))
                     raise er
-
+                except CheckNotComputedError as er:
+                    self.set_text("Element could not be computed", full_tag)
+                    failed_tags.append(er.get_check)
+                    continue
                 self.merge_template(result, full_tag)
 
-        return self.template
+            
+        return self.template, failed_tags
 
 
 # TagExpression represents the literal tag as it occurs in the template
@@ -569,3 +613,19 @@ class TagChaining:
             raise NotImplementedError(f"Tag generation for instance of '{type(last_tag).__name__}' is not supported.")
 
         return GeneratedTag
+
+#processes errors as tags
+class ErrorLeafTag(LeafTag):
+        def __init__(self, key):
+            super().__init__(self.process_tag, Null, 0)
+            self.key = key
+            self.set_required_data_field(key)
+
+        def process_tag(self, _):
+            return str(self.key)
+
+        def validate_and_process(self, data):
+            return str(self.key)
+
+def generate_error_leaf_tag(key):
+    return ErrorLeafTag(key)

backend/dqt/tools/tags/template_tags/dataset.py
@@ -26,7 +26,7 @@ from dqt.tools.tags.leaf_tags.dataset.value import ValueLeafTag
 from dqt.tools.tags.leaf_tags.examples_leaf_tag_factory import generate_examples_leaf_tag
 from dqt.tools.tags.leaf_tags.key_leaf_tag_factory import generate_key_leaf_tag
 from dqt.tools.tags.tag import TemplateTag
-
+from dqt.tools.errors import CheckNotComputedError
 
 class DatasetTemplateTag(TemplateTag):
     CHECK_MAPPING = {
@@ -124,7 +124,12 @@ class DatasetTemplateTag(TemplateTag):
                 self.set_sub_tag("totalOcidCount", generate_key_leaf_tag("totalOcidCount"))
                 self.set_sub_tag("totalBuyerCount", generate_key_leaf_tag("totalBuyerCount"))
                 self.set_sub_tag("examples", generate_examples_leaf_tag("examples"))
-
+        else: #check not found
+            raise CheckNotComputedError(
+                reason="Check was not computed",
+                check=check_name
+            )
+            
         super().finalize_params()
 
     def prepare_data(self, data):

backend/dqt/views.py
@@ -296,7 +296,8 @@ def generate_report(request):
         base = BaseTemplateTag(gdocs, input_message["dataset_id"])
         base.set_param("template", input_message["document_id"])
         base.finalize_params()
-        main_template = base.validate_and_process({})
+        failed_tags = []
+        main_template, failed_tags = base.validate_and_process({})
 
         report_name = "Report %s %s" % (input_message["dataset_id"], datetime.now())
         if "report_name" in input_message and isinstance(input_message["report_name"], str):
@@ -304,7 +305,13 @@ def generate_report(request):
 
         file_id = gdocs.upload(input_message["folder_id"], report_name, main_template)
 
-        response = JsonResponse({"status": "ok", "data": {"file_id": file_id}})
+        response = JsonResponse(
+            {
+                "status": "ok", 
+                "data": {"file_id": file_id},
+                "failed_tags": failed_tags
+                }
+            )
     except GoogleDriveError as er:
         response = JsonResponse({"status": "report_error", "data": {"reason": str(er)}})
     except TagError as er:
@@ -312,6 +319,7 @@ def generate_report(request):
             {
                 "status": "template_error",
                 "data": [er.as_dict()],  # Can accommodate multiple TagErrors in the future
+                "failed_tags" : failed_tags
             }
         )
     finally:

frontend/src/messages/en.js
@@ -14,7 +14,7 @@ export const messages = {
     close: "Close window",
     created: "Created",
     modified: "Modified",
-    core: {
+    core: {         
         passedExamples: "Sample of successes",
         failedExamples: "Sample of failures",
         undefinedExamples: "Sample of unprocessed data"
@@ -56,7 +56,8 @@ export const messages = {
             ok: "Report has been successfully created.",
             templateError: "Template syntax is invalid.",
             reportError: "Report creation has failed.",
-            serverError: "An unexpected error has occurred. Please contact the support so it can be resolved."
+            serverError: "An unexpected error has occurred. Please contact the support so it can be resolved.",
+            warning: "Some elements were not computed. Please check your template and dataset."
         },
         link: "Link",
         reason: "Reason",
@@ -67,7 +68,9 @@ export const messages = {
         reportName: "Report name",
         reportNameTooltip: "<p>(optional) name of the the final report on Google Drive&#8482;</p>",
         submit: "Submit",
-        errorReport: "Error report"
+        errorReport: "Error report",
+        warningList: "List of checks, which were not computed",
+        warningEnd: "We recommend you to check your dataset and template to generate complete report."
     },
     datasetValuesMultiselect: {
         noResult: "No values found. Consider changing the search query.",

commit 9978dd99488a1df78735d0f69b32bae56c189d36

fix: Fix bug introduced in ff00c21, if API returns "report_error" response

frontend/src/components/DatasetReportModal.vue
@@ -404,8 +404,11 @@ export default {
             axios
                 .post(`${CONFIG.apiBaseUrl}${CONFIG.apiEndpoints.createDatasetReport}`, data)
                 .then(response => {
-                    if (response.data.failed_tags.length == 0) this.failedTags = null;
-                    else this.failedTags = response.data.failed_tags;
+                    if (response.data.failed_tags && response.data.failed_tags.length == 0) {
+                        this.failedTags = null;
+                    } else {
+                        this.failedTags = response.data.failed_tags;
+                    }
 
                     if (response.status == 200) {
                         this.submitStatus = response.data.status;