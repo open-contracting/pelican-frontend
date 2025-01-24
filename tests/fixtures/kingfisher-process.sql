cfdf67fb8118042eb2bcaa9d8f32ce63450ec566
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-01-24 11:49:40 -0500

    ci: Print out SQL tables

--- tests/fixtures/kingfisher-process.sql
+++ tests/fixtures/kingfisher-process.sql
@@ -1,10 +1,10 @@
-CREATE TABLE compiled_release (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS compiled_release (
+    id bigserial PRIMARY KEY,
     ocid text NOT NULL,
     collection_id bigint NOT NULL,
+    collection_file_id bigint NOT NULL,
     data_id bigint NOT NULL,
-    release_date text NOT NULL,
-    collection_file_id bigint NOT NULL
+    release_date text NOT NULL
 );
 
-INSERT INTO compiled_release VALUES (1, 'ocds-213czf-1', 123, 1, '2000-01-01T00:00:00Z', 1);
+INSERT INTO compiled_release VALUES (1, 'ocds-213czf-1', 123, 1, 1, '2000-01-01T00:00:00Z');
