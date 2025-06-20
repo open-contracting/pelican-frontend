496031e3adda10e68869b53b653faa1eb84c8cd9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-06-20 00:35:32 -0400

    fix: Use 127.0.0.1 to avoid IPv6 error log messages

--- core/settings.py
+++ core/settings.py
@@ -270,7 +270,7 @@ FATHOM = {
 }
 
 # The connection string for RabbitMQ.
-RABBIT_URL = os.getenv("RABBIT_URL", "amqp://localhost")
+RABBIT_URL = os.getenv("RABBIT_URL", "amqp://127.0.0.1")
 # The name of the RabbitMQ exchange used by Pelican backend.
 RABBIT_EXCHANGE_NAME = os.getenv("RABBIT_EXCHANGE_NAME", "pelican_development")
 
