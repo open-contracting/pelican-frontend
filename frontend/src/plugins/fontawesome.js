7d1ab4eff2d11848cb62f7c4d9364ae661b4303f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-02 00:57:09 -0500

    build(vue-fontawesome): Register the component in main.js. Layers is not used (see also FortAwesome/Font-Awesome#19344).

--- frontend/src/plugins/fontawesome.js
+++ frontend/src/plugins/fontawesome.js
@@ -1,12 +1,9 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from "@fortawesome/vue-fontawesome";
-import Vue from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(fas);
 library.add(far);
 
-Vue.component("FontAwesomeIcon", FontAwesomeIcon);
-Vue.component("FontAwesomeLayers", FontAwesomeLayers);
-Vue.component("FontAwesomeLayersText", FontAwesomeLayersText);
+export { FontAwesomeIcon };
