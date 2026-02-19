20e581ccc86e86d808ff37e92dae353af62036fb
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-02-19 15:52:41 -0500

    chore: Remove unused formatters

--- frontend/src/composables/useFormatters.js
+++ frontend/src/composables/useFormatters.js
@@ -8,28 +8,9 @@ export function useFormatters() {
         return numeral(value).format("0,0");
     };
 
-    const formatNumber2D = (value) => {
-        return numeral(value).format("0,0.00");
-    };
-
     const formatPercentage = (value) => {
         const rounded = Math.round(value);
 
-        if (rounded === 0.0 && value !== 0.0) {
-            return `>${numeral(rounded).format("0")}%`;
-        }
-        if (rounded === 100.0 && value !== 100.0) {
-            return `<${numeral(rounded).format("0")}%`;
-        }
-        return `${numeral(rounded).format("0")}%`;
-    };
-
-    const formatPercentageOmitZero = (value) => {
-        const rounded = Math.round(value);
-
-        if (value === 0.0) {
-            return "";
-        }
         if (rounded === 0.0 && value !== 0.0) {
             return `>${numeral(rounded).format("0")}%`;
         }
@@ -53,9 +34,7 @@ export function useFormatters() {
 
     return {
         formatNumber,
-        formatNumber2D,
         formatPercentage,
-        formatPercentageOmitZero,
         formatPercentage2D,
     };
 }
